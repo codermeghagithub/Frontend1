3cef576162f0ab63e4ed39d8b063aaad7c66a7a6
codermeghagithub <meghade2003@gmail.com>
2025-08-04 18:33:07 +0530

    add few files and implement function

---
+++ Frontend_1/Typescript/Day-2/dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+function greet(username) {
+    return `Hello ${username}` + 1;
+}
+console.log(greet("Megha"));
---
+++ Frontend_1/Typescript/Day-2/index.ts
@@ -0,0 +1,5 @@
+function greet(username:String):string{
+  return `Hello ${username}`+1;
+
+}
+console.log(greet("Megha"));
