c2fadd75aab991efcad3ebae7e546348ff46c529
codermeghagithub <meghade2003@gmail.com>
2025-08-10 09:31:08 +0530

    add some files

--- Frontend_1/React_js/src/Useref.jsx
+++ Frontend_1/React_js/src/Useref.jsx
@@ -20,7 +20,15 @@ const onChangeColor=()=>{
     <div className='flex flex-col items-center justify-center h-screen bg-zinc-800 text-white'>
 <div className='flex flex-row items-center justify-center gap-4'>
 
+<button onClick={()=>setCount(count+1)}
+className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>+ Add
+</button>
+<span className="text-2xl font-bold">{count}</span>
+<button onClick={()=>setCount(count-1)}
+className='bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded'>+ Remove
+</button>
 
+<button ref={btnRef} onClick={onChangeColor}>change color to red</button>
     </div>
     </div>
     
