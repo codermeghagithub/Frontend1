0f04293bd45361665b19ab85911ecc6ae4847ed1
codermeghagithub <meghade2003@gmail.com>
2025-08-15 00:14:31 +0530

    properly light and dark mode implementation

--- Frontend_1/Javascript/Miniproject/dark_light.js
+++ Frontend_1/Javascript/Miniproject/dark_light.js
@@ -1,16 +1,21 @@
 const  body=document.getElementsByTagName("body")[0];
 const toggleBtn=document.getElementsByTagName("button")[0];
-const isDarkMode=false;
+let isDarkMode=false;
   // console.log(isDarkMode);
 
 toggleBtn.addEventListener("click",()=>{
+  console.log(isDarkMode);
+
+
   if(isDarkMode){
-    body.classList.remove("dark");
+    body.classList.add("light");
     isDarkMode=false;
+    body.classList.remove("dark")
   }
   else{
       body.classList.add("dark");
       isDarkMode=true;
+      body.classList.remove("light")
   }
 
 })
