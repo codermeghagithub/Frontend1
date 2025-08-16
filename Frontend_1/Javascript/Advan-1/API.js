f7199f2dbcf16333c219c43ebda70fd0700f3c0f
codermeghagithub <meghade2003@gmail.com>
2025-08-16 10:54:01 +0530

    Refactor API.js to comment out fetch example and add APIrequest.html for XMLHttpRequest demonstration

--- Frontend_1/Javascript/Advan-1/API.js
+++ Frontend_1/Javascript/Advan-1/API.js
@@ -1,4 +1,7 @@
   //* here .then is a function hwo pass afunction using callback
-fetch('https://dummyjson.com/products')
-.then((res)=>res.json())
-.then((data)=>console.log(data))
+// fetch('https://dummyjson.com/products')
+// .then((res)=>res.json())
+// .then((data)=>console.log(data))
+
+
+
