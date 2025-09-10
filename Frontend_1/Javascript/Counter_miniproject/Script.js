150eda811d7c2506c9a82096e3937c5061c6f6ed
codermeghagithub <meghade2003@gmail.com>
2025-09-10 21:49:10 +0530

    add one miniproject counter

--- Frontend_1/Javascript/Counter_miniproject/Script.js
+++ Frontend_1/Javascript/Counter_miniproject/Script.js
@@ -1,2 +1,25 @@
+const count=document.querySelector('.count')
 const minasbtn=document.querySelector('#minas')
-const plasbtn=document.querySelector('#plas')
+const plasbtn=document.querySelector('#plus')
+const changeby=document.querySelector('.changeval')
+const previous=document.querySelector('#set')
+
+minasbtn.addEventListener('click',()=>{
+const coutvalue=parseInt(count.innerText);
+// count.innerText=countminus-1;
+const changevalue=parseInt(changeby.value)
+count.innerText=coutvalue-changevalue;
+
+})
+
+plasbtn.addEventListener("click",()=>{
+const coutvalue=parseInt(count.innerText)  
+// count.innerText=countval+1;
+const changevalue=parseInt(changeby.value)
+count.innerText=coutvalue+changevalue;
+
+})
+
+previous.addEventListener('click',()=>{
+  count.innerText=0;
+})
